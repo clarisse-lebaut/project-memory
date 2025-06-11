770016df04dfd9222ce683d6e7112bf05c69183d
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2025-06-11 12:41:54 +0200

    feat : ajout de nouveau alias pour faciliter l'import d'éléments (style et image)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@css": path.resolve(__dirname, "./src/assets/styles"),
+      "@illustration": path.resolve(__dirname, "./src/assets/img/illustrationCard"),
+    },
+  },
+});

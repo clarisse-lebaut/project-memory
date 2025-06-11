3ea469ceaa3bfbfc604970216fb683c2ae71bbb5
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2025-06-11 17:00:22 +0200

    feat : ajout du composant bouton

---
+++ src/component/global/Button.jsx
@@ -0,0 +1,11 @@
+function Button({ text, onClick, className }) {
+  return (
+    <>
+      <button onClick={onClick} className={className}>
+        {text}
+      </button>
+    </>
+  );
+}
+
+export default Button;

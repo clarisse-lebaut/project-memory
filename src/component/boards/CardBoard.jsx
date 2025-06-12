088d3d6dd78a4bf962695cdc0b9bb9ad48851a5d
Clarisse LE BAUT <clarisse.lebaut@gmail.com>
2025-06-12 22:23:29 +0200

    feat : changement d'état pour le départ de la partie, ajout de changement d'état de l'état du clic lors du relancement de la session de jeu + des cartes selectionner, récupération des cartes sélectionner + le match entre les cartes + ajustement de leur état afin d'avoir une visibilité UI, ajout d'un nouveau state pour déterminer quand la partie est commencé,

--- src/component/boards/CardBoard.jsx
+++ src/component/boards/CardBoard.jsx
@@ -9,8 +9,9 @@ const duplicateCard = (cards, count = numberDuplication) =>
 
 function CardBoard() {
   const [cards, setCards] = useState(duplicateCard(initialCardImage, numberDuplication));
-  const [disable, setDisable] = useState(false);
   const [selected, setSelected] = useState([]);
+  const [disable, setDisable] = useState(true);
+  const [gameStarted, setGameStarted] = useState(false);
 
   const flipCard = (index) => {
     if (disable) return;
@@ -26,6 +27,7 @@ function CardBoard() {
 
     setCards(schuffleCards);
     setDisable(false);
+    setGameStarted(true);
   };
 
   const seeCards = () => {
@@ -50,47 +52,67 @@ function CardBoard() {
   };
 
   const rebootGameSession = () => {
+    setDisable(true);
     setCards(duplicateCard(initialCardImage, numberDuplication));
+    setGameStarted(false);
   };
 
   const selectCard = (card) => {
+    if (disable) return;
+
     console.log(card.nameCard);
 
     setSelected((prevSelectedCards) => {
       const newSelectedCards = [...prevSelectedCards, card];
 
       if (newSelectedCards.length === numberDuplication) {
-        compareCards(newSelectedCards);
-        // Réinitialiser les cartes sélectionnées après la comparaison
+        const selectedCard = newSelectedCards;
+        if (selectedCard[0].nameCard === selectedCard[1].nameCard) {
+          setCards((prevSelectedCards) =>
+            prevSelectedCards.map((card) =>
+              card.nameCard === selectedCard[0].nameCard
+                ? { ...card, flipped: true, matched: true }
+                : card
+            )
+          );
+        } else {
+          setDisable(true);
+
+          setTimeout(() => {
+            setCards((prevCards) =>
+              prevCards.map((card) => (card.matched ? card : { ...card, flipped: false }))
+            );
+            setDisable(false);
+          }, 1000);
+        }
+
         return [];
       }
       return newSelectedCards;
     });
   };
 
-  const compareCards = (card) => {
-    if (card[0].nameCard === card[1].nameCard) {
-      console.log("Il faut que les cartes restent de face.");
-    } else {
-      console.log("Il faut que les cartes se retournent.");
-      setTimeout(
-        () => setCards((prevCards) => prevCards.map((card) => ({ ...card, flipped: false }))),
-        1000
-      );
-    }
-  };
-
   const handleClick = (index, card) => {
+    if (card.matched) return;
     flipCard(index);
     selectCard(card);
   };
 
   return (
     <section>
-      <Button text="Commencer une nouvelle partie" onClick={schuffleCards} />
-      <Button text="Stoper tout, j'en ai marre." onClick={cancelGame} />
-      <Button text="Un coup de main ?" onClick={seeCards} />
-      <Button text="Reboot la session de jeu" onClick={rebootGameSession} />
+      {!gameStarted ? (
+        <Button
+          text={!gameStarted ? "Commencer la partie" : "Recommencer la partie"}
+          onClick={schuffleCards}
+        />
+      ) : (
+        <>
+          <Button text="Recommencer la partie." onClick={schuffleCards} />
+          <Button text="Arrêter la partie." onClick={rebootGameSession} />
+          <Button text="Un coup de main ?" onClick={seeCards} />
+          <Button text="J'arrête, je veux voir la solution." onClick={cancelGame} />
+        </>
+      )}
 
       <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
         {cards.map((card, index) => (
